3b15ac7afb0a6874037bb7bb3a23c05498955c96
Manjeet Chhikara <mchhikara@Manjeets-MBP.attlocal.net>
2023-09-13 18:27:40 -0400

    Adding actuator

--- src/main/java/com/manjeet/learnspring/data/entity/Room.java
+++ src/main/java/com/manjeet/learnspring/data/entity/Room.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Room {
     @Id
     @Column(name = "ROOM_ID")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long roomid;
 
     @Column(name = "NAME")
