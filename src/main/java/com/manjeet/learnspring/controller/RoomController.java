dcd1a78a9d19c0646e231b7fc878a5a9c7689c9e
Manjeet Chhikara <mchhikara@Manjeets-MBP.attlocal.net>
2023-09-23 23:05:41 -0400

    GET  operation

--- src/main/java/com/manjeet/learnspring/controller/RoomController.java
+++ src/main/java/com/manjeet/learnspring/controller/RoomController.java
@@ -3,12 +3,13 @@ package com.manjeet.learnspring.controller;
 import com.manjeet.learnspring.data.entity.Room;
 import com.manjeet.learnspring.data.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/rooms")
 public class RoomController {
 
@@ -16,8 +17,12 @@ public class RoomController {
     private RoomRepository roomRepository;
 
     @GetMapping
-    @ResponseBody
-    public Iterable<Room> getRooms() {
-        return this.roomRepository.findAll();
+    public ResponseEntity<Iterable<Room>> getRooms() {
+        return ResponseEntity.ok(this.roomRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Room> getRoom(@PathVariable("id") long roomId) {
+        return ResponseEntity.ok(this.roomRepository.findById(roomId).get());
     }
 }
